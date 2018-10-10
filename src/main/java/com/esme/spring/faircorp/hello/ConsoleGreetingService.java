d2bd4356fca14aaac9dcb3d5e46eeeffa5d4ab40
7totor7 <nestor.hernandez@etu.emse.fr>
2018-10-10 16:12:02 +0200

    DataBase implemented

--- src/main/java/com/esme/spring/faircorp/hello/ConsoleGreetingService.java
+++ src/main/java/com/esme/spring/faircorp/hello/ConsoleGreetingService.java
@@ -1,5 +1,9 @@
 package com.esme.spring.faircorp.hello;
 
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class ConsoleGreetingService implements GreetingService {
     @Override
     public void greet(String name){
--- src/main/java/com/esme/spring/faircorp/hello/DummyUserService.java
+++ src/main/java/com/esme/spring/faircorp/hello/DummyUserService.java
@@ -1,5 +1,23 @@
 package com.esme.spring.faircorp.hello;
 
-public class DummyUserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+
+public class DummyUserService implements UserService {
+
+    @Autowired
+    GreetingService gs;
+
+    @Override
+    public void greetAll() {
+        List<String> users = Arrays.asList("Elodie", "Charles");
+        users.forEach(name -> gs.greet(name));
+
+    }
 
 }

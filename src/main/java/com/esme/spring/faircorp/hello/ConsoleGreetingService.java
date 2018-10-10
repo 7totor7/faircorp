e9c9ab153a920870b95a5a7a1dba3b0e3a304588
7totor7 <nestor.hernandez@etu.emse.fr>
2018-10-10 12:11:35 +0200

    UserService interface created

--- src/main/java/com/esme/spring/faircorp/hello/ConsoleGreetingService.java
+++ src/main/java/com/esme/spring/faircorp/hello/ConsoleGreetingService.java
@@ -1,13 +1,8 @@
 package com.esme.spring.faircorp.hello;
 
-public class ConsoleGreetingService {
-    public void sayHello() {
-        GreetingService greetingService = new GreetingService() {
-        };
-        System.out.println("Hello " +
-                greetingService.greet();
-    }
-
-    public void greet(String spring) {
+public class ConsoleGreetingService implements GreetingService {
+    @Override
+    public void greet(String name){
+        System.out.println("Hello, " + name + "!");
     }
 }

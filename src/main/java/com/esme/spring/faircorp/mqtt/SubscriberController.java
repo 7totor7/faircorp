75795e101bfe340711f78fe6b16e2253c56450f4
7totor7 <nestor.hernandez@etu.emse.fr>
2018-12-20 12:29:39 +0100

    mqtt subscriber finished

--- src/main/java/com/esme/spring/faircorp/mqtt/SubscriberController.java
+++ src/main/java/com/esme/spring/faircorp/mqtt/SubscriberController.java
@@ -1,7 +1,6 @@
 package com.esme.spring.faircorp.mqtt;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +13,18 @@ import java.net.URISyntaxException;
 @RequestMapping("/api/subscribe/") // (2)
 @Transactional // (3)
 public class SubscriberController {
+
+    public SubscriberController() throws MqttException, URISyntaxException {
+    }
+
+    //Subscriber out of Getmapping to avoid creating again the same subscriber.
+    private Subscriber s = new Subscriber("tcp://7totor7:7totor7@m15.cloudmqtt.com:14768");
+
+    //Inital Get when loading the page
     @CrossOrigin
     @GetMapping // (5)
-    public String subscribe() throws MqttException, URISyntaxException, InterruptedException {
-        Subscriber s = new Subscriber("tcp://7totor7:7totor7@m15.cloudmqtt.com:14768");
-        String message = "[{\"id\":-2,\"level\":0,\"status\":\"OFF\",\"roomId\":-10},{\"id\":-1,\"level\":8,\"status\":\"ON\",\"roomId\":-10}]";
+    public String subscribe() throws MqttException, InterruptedException {
+        String message = "[{\"id\":0,\"level\":8,\"status\":\"ON\",\"roomId\":0},{\"id\":1,\"level\":0,\"status\":\"OFF\",\"roomId\":1}]";
         s.sendMessage(message);
         //Wait until the message is received to return last_message.
         //received_complete and last_message are public var from the subscriber class.

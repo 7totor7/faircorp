72e46f0c780d168497844f8512ef23d87b201ca0
7totor7 <nestor.hernandez@etu.emse.fr>
2019-01-07 18:13:53 +0100

    switch

--- src/main/java/com/esme/spring/faircorp/model/LightController.java
+++ src/main/java/com/esme/spring/faircorp/model/LightController.java
@@ -1,6 +1,5 @@
 package com.esme.spring.faircorp.model;
 
-import com.esme.spring.faircorp.mqtt.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
